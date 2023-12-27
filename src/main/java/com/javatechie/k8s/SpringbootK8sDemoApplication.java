6e3224c6793bb2c137e4e66f5702cc7afddf9d03
aymen drira <sjdfmoizjef@gmail.com>
2023-12-27 14:57:22 +0100

    Initial Commit

--- src/main/java/com/javatechie/k8s/SpringbootK8sDemoApplication.java
+++ src/main/java/com/javatechie/k8s/SpringbootK8sDemoApplication.java
@@ -9,13 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SpringbootK8sDemoApplication {
 
-	@GetMapping("/message")
-	public String displayMessage(){
-		return "Congratulation you successfully deployed your application to kubernetes !!";
-	}
+    @GetMapping("/message")
+    public String displayMessage() {
+        return "Congratulation you successfully deployed your application to kubernetes !!";
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootK8sDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootK8sDemoApplication.class, args);
+    }
 
 }
